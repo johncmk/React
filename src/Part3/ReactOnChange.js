9f1d00fa8df169ed7231ec1ea37a43c742a1a937
johnck <johnck408@gmail.com>
2024-01-27 22:15:21 -0500

    Rename ReactChange file to ReactOnChange and added some comments

--- src/Part3/ReactOnChange.js
+++ src/Part3/ReactOnChange.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
-class ReactonChange extends Component 
+/**
+ * react에서는 onChange 이벤트도 camelCase 형식의 명칭을 사용한다.
+ * onChange 이벤트는 특정 element에 변화가 생겼을 때 정의된 함수를 호출하는
+ * 방식으로 사용한다.
+ */
+
+class ReactOnChange extends Component 
 {
     change = (e) => {
         var val = e.target.value;
@@ -21,4 +27,4 @@ class ReactonChange extends Component
     }
 }
 
-export default ReactonChange;
+export default ReactOnChange;
