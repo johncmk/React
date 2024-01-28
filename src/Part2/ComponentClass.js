6dffa365b4827853bff7c4cedbf939a82734dc46
johnck <johnck408@gmail.com>
2024-01-27 21:51:26 -0500

    Added comment at src/Part2/ComponentClass.js

--- src/Part2/ComponentClass.js
+++ src/Part2/ComponentClass.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react'
 
+
+/**
+ * props는 부모 컴포넌트가 자식 컴포넌트에 데이터를 전달할 때 사용한다. props를 전달받은 자식
+ * 컴포넌트에서는 데이터를 수정할 수 없다. 데이터를 변경하기 위해서는 컴포넌트 내부에서만 사용하는
+ * 변수에 값을 넣어 사용해야한다.
+ * 
+ * Props를 상위 컴포넌트에서 하위 컴포넌트로 데이터를 전달할 때 사용했다면, state는 하나의
+ * 컴포넌트 안에서 전역 변수처럼 사용한다.
+ */
 class ComponentClass extends Component 
 {
     constructor(props)
